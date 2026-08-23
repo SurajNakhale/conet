2fae7803f720ebb6235dc46168c2084bea01ed99
suraj <surajnakhale2407@gmail.com>
2026-08-24 01:19:30 +0530

    feat: add ws message schemas

--- packages/types/src/types.ts
+++ packages/types/src/types.ts
@@ -13,3 +13,25 @@ export const signinSchema = z.object({
 export const createRoomSchema = z.object({
   name: z.string().min(3).max(12)
 })
+
+
+export const authSchema = z.object({
+  type: z.literal("auth"),
+  payload: z.object({
+    token: z.jwt()
+  })
+})
+
+export const joinSchema = z.object({
+  type: z.literal("join"),
+  payload: z.object({
+    roomId: z.uuid()
+  })
+})
+
+export const chatSchema = z.object({
+  type: z.literal("chat"),
+  payload: z.object({
+    text: z.string().trim().min(1)
+  })
+})
