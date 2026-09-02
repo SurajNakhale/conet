52c0fbd9452568d9c74c9bcc7a74a7d2a8f8387d
suraj <surajnakhale2407@gmail.com>
2026-09-02 19:30:56 +0530

    feat(http): add routes

--- apps/http-server/src/routes/route.ts
+++ apps/http-server/src/routes/route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { getuser, signin, signup } from "../controllers/authController";
-import { createRoom, deleteRoom, getAllRooms, getMessage } from "../controllers/roomController";
+import { getuser, getUserbyId, signin, signup } from "../controllers/authController";
+import { createRoom, deleteRoom, getAllRooms, getMessage, getRoomById } from "../controllers/roomController";
 import { authMiddleware } from "../middleware/authMiddleware";
 
 
@@ -14,8 +14,10 @@ router.post('/signin', signin);
 router.post('/room', authMiddleware, createRoom);
 
 router.get('/user', authMiddleware, getuser);
-router.get('/rooms', getAllRooms);
+router.get('/rooms', authMiddleware, getAllRooms);
 router.get('/room/:roomId/messages', authMiddleware, getMessage);
+router.get('/user/:id', authMiddleware, getUserbyId);
+router.get('/room/:id', authMiddleware, getRoomById);
 
 router.delete('/room/:roomId', authMiddleware, deleteRoom);
 
