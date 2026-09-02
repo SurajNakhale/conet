8d42828faf67f05d54f075276f59fb842519cf57
suraj <surajnakhale2407@gmail.com>
2026-09-02 19:15:09 +0530

    refactor: decrease pasword min len

--- packages/types/src/types.ts
+++ packages/types/src/types.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const signupSchema = z.object({
   username: z.string().min(3).max(30),
-  password: z.string().min(8),
+  password: z.string().min(4),
 });
 
 export const signinSchema = z.object({
