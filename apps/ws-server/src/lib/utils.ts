c912c9e3fc1b1f160236dfd6c4e6715a78da80af
suraj <surajnakhale2407@gmail.com>
2026-09-02 19:24:52 +0530

    refactor(ws): add members field (array of userId) to reponse field

--- apps/ws-server/src/lib/utils.ts
+++ apps/ws-server/src/lib/utils.ts
@@ -1,16 +1,23 @@
-import { Rooms } from "../state";
+import { Rooms, SocketToUserId } from "../state";
 
 export function countMembers(roomId: string){
-    const members = Rooms.get(roomId)?.size ?? 0;
-        console.log(members);
+    const memberCount = Rooms.get(roomId)?.size ?? 0;
+    const sockets = Rooms.get(roomId)
+    const userIds = new Set();
+
+     SocketToUserId.forEach((userId, socket) => {
+        if(sockets!.has(socket)){
+            userIds.add(userId); 
+        }
+    })
 
         Rooms.get(roomId)?.forEach((ws) => {
             ws.send(JSON.stringify({
                 type: "room_size",
                 payload: {
-                    totalUsers: members
+                    memberCount: memberCount,
+                    members: [...new Set(userIds)],
                 }
             }));
-
         })   
 }
