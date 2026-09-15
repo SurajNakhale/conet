18286699ffa523fd9cffe754492c579fab0f5849
suraj <surajnakhale2407@gmail.com>
2026-09-15 20:32:01 +0530

    feat(http-server):  configure cors

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -2,16 +2,26 @@ import express from "express";
 import router from "./routes/route";
 import { errorMiddleware } from "./middleware/errorMiddleware";
 import cors from "cors";
+import "dotenv/config";
+
 
 const app = express();
-
+const port = Number(process.env.PORT) || 4000;
+const frontendUrl = process.env.FRONTEND_URL;
+console.log(frontendUrl)
 app.use(express.json());
-app.use(cors())
+
+app.use(cors({
+    origin: frontendUrl,
+    methods: ["GET, POST, PUT, DELETE, PATCH"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 app.use('/api', router);
 
 app.use(errorMiddleware);
 
-app.listen(4000, () => {
-    console.log("http server running on port 4000");
+
+app.listen(port, () => {
+    console.log(`http server running on port ${port}`);
 });
